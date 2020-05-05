df360bf6c5da554fc410b561845cda3be33980c0
Foreverowlone <60590212+Foreverowlone@users.noreply.github.com>
2020-05-05 01:49:07 -0700

    Added new map, changed waypoints, and added scripts.

---
+++ Assets/Scripts/OnClickDestroy.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OnClickDestroy : MonoBehaviour
+{
+    public int score;
+    public int health = 2;
+    public int coinsTotal;
+    public int coins;
+    public Text purseCoins;
+    public Text scoreTotal;
+    public Slider healthStat;
+    public GameObject defense;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        purseCoins = GameObject.Find("coinsTotal").GetComponent<Text>();
+        healthStat = GameObject.Find("SliderHealth").GetComponent<Slider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Destroy(gameObject);
+            if (gameObject.tag == "EnemyA")
+            {
+                coins += 2;
+            }
+            if (gameObject.tag == "EnemyB")
+            {
+                coins += 3;
+            }
+
+            scoreTotal.text = score.ToString();
+        }
+    }
+}
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    private int score = 0;
+    private int hScore = 0;
+    public Text pScore;
+    public Text hText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pScore.text = score.ToString("D4");
+        hText.text = hScore.ToString("D4");
+
+        pScore.text = score.ToString();
+
+        if (score > hScore)
+        {
+            hScore = score;
+        }
+    }
+
+    public void setScore(int pScore)
+    {
+        score = pScore;
+    }
+
+    public int getScore()
+    {
+        return score;
+    }
+
+    public void setHScore(int h)
+    {
+        hScore = h;
+    }
+
+    public int getHScore()
+    {
+        return hScore;
+    }
+
+}
